08ac0a6bc4a7c215658e141167bbb590babee1b5
1DANAPAULA <wanderleyanapaula2009@gmail.com>
2025-06-05 16:44:15 -0300

    Landing page finalizado

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
+//Busca um elemento no HTML e salva em uma variavel 
 let menu = document.getElementById("menu")
-let iconebarras = document.getElementById("icone-barras")
-let iconeX = document.getElementById("icone-X")
+let iconeBarras = document.getElementById("icone-barras")
+let iconeX = document.getElementById("icone-x")
 
+//Função que vai abrir e fechar o menu 
 function abreFechaMenu(){
-
-    //Se o menu está fechado
+    
+    //Se o menu está fechado 
     if(menu.classList.contains("menu-fechado")){
-        //Abrir menu
+        //Abrir o menu
         menu.classList.remove("menu-fechado")
 
         //Mostra icone X
         iconeX.style.display = "inline"
 
-        //Esconder o icone barras
-        iconebarras.style.display = "none"
-
-
+        //Esconder o icone Barras
+        iconeBarras.style.display = "none"
     }
     //Senão
     else{
-    //Fechar o menu
-    menu.classList.add("menu-fechado")
+        //Fechar o menu
+        menu.classList.add("menu-fechado")
 
-    //Esconder icone X
-    iconeX.style.display = "none"
+        //Esconder icone x
+        iconeX.style.display = "none"
 
-    //Mostrar icone barras
-    iconebarras.style.display = "inline"
+        //Mostrar icone barras
+        iconeBarras.style.display = "inline"
+    }
+}
 
-   }
- }
-
-onresize =() => {
+onresize = () => {
     menu.classList.remove("menu-fechado")
 
     //Mostrar icone X
     iconeX.style.display = "inline"
 
     //Esconder icone barras
-    iconebarras.style.display = "none"
+    iconeBarras.style.display = "none"
 }
